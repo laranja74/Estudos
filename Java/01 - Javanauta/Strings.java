9622a285f6ee2569e2d1a3ea24cb0d4bea326bcf
Gabriel <gabriel.nascimento7004@gmail.com>
2025-11-16 16:28:49 -0300

    att

--- Java/01 - Javanauta/Strings.java
+++ Java/01 - Javanauta/Strings.java
@@ -3,12 +3,16 @@ public class Strings {
 
         String nome = "Gabriel";
         String segundoNome = "Letícia";
+        String concatenacao = nome.concat(segundoNome);
 
         int tamanhoString = nome.length();
         boolean equals = nome.equals(segundoNome);
 
+
         System.out.println("O nome do primeiro participante é " + nome.toUpperCase() +" e o nome do segundo é " + segundoNome.toUpperCase() );
         System.out.println("Seus nomes são iguais? " + equals);
+        System.out.println("Tamanho da String: " + tamanhoString);
+        System.out.println("Tamanho da String: " + concatenacao.length() + " com o nome completo");
 
     }
 }
