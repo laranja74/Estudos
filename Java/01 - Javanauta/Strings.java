ae1f3748c1b7464c29e1c114de4da58e9958214c
Gabriel <gabriel.nascimento7004@gmail.com>
2025-11-16 11:37:07 -0300

    Create Strings.java

---
+++ Java/01 - Javanauta/Strings.java
@@ -0,0 +1,15 @@
+public class Strings {
+    public static void main(String[] args) {
+        // Exemplos de manipulação de strings em Java
+        String nome = "Gabriel";
+        String sobrenome = "Nascimento";
+        String nomeCompleto = nome + " " + sobrenome;
+        String nomeUltimo = "Silva";
+        String saudacao = "Olá, " + nomeCompleto + "! Seja bem-vindo ao mundo Java.";
+        System.out.println("Olá " + nome + ", vejo que seu sobrenome é " + sobrenome + " " + nomeUltimo + ", correto?");
+        System.out.println("Seu nome completo é " + nomeCompleto + ", correto?");
+        System.out.println(saudacao);
+
+
+    }
+}
