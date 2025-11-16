e393c989c25a58b32ce4a43da3838097d52b5490
Gabriel <gabriel.nascimento7004@gmail.com>
2025-11-16 00:22:28 -0300

    att

--- Java/01 - Javanauta/OperadoresAritméticos.java
+++ Java/01 - Javanauta/OperadoresAritméticos.java
@@ -6,17 +6,15 @@ public class OperadoresAritméticos {
         // 3. Multiplicação (*)
         // 4. Divisão (/)
         // 5. Módulo (%)
-        double num1 = 15.5;
-        double num2 = 4.5;
-        double soma = num1 + num2;
-        double subtracao = num1 - num2;
-        double multiplicacao = num1 * num2;
-        double divisao = num1 / num2;
-        double modulo = num1 % num2;
-        System.out.println("Soma: " + soma);
-        System.out.println("Subtração: " + subtracao);
-        System.out.println("Multiplicação: " + multiplicacao);
-        System.out.println("Divisão: " + divisao);
-        System.out.println("Módulo: " + modulo);    
+        double energia = 83.4;
+        double agua = 30.5;
+        double internet = 80.0;
+        double totalContas = energia + agua + internet;
+        double energiaAumento = energia * 1.05; // Aumento de 5% na conta de energia
+        double mediaContas = totalContas / 3;
+
+        System.out.println("Total das contas: " + totalContas);
+        System.out.println("Conta de energia após aumento: " + energiaAumento);
+        System.out.println("Média das contas: " + mediaContas);
     }
 }
