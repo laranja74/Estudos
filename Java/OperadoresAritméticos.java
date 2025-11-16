7e299c7ee7ffea45dd2fdd06ad493d76645878c5
Gabriel <gabriel.nascimento7004@gmail.com>
2025-11-15 23:29:21 -0300

    Inicio de uma grandeza
    Começando por JAVA!

---
+++ Java/OperadoresAritméticos.java
@@ -0,0 +1,11 @@
+public class OperadoresAritméticos {
+    public static void main(String[] args) {
+        // Exemplos de operadores aritméticos em Java
+        // 1. Adição (+)
+        // 2. Subtração (-)
+        // 3. Multiplicação (*)
+        // 4. Divisão (/)
+        // 5. Módulo (%)
+        
+    }
+}
---
+++ Java/tiposPrimitivos.java
@@ -0,0 +1,18 @@
+public class tiposPrimitivos {
+    public static void main(String[] args) {
+        // Tipos primitivos em Java:
+        // 1. Números inteiros (byte, short, int, long)
+        // 2. Números de ponto flutuante (float, double)
+        // 3. Caracteres (char)
+        // 4. Booleanos (boolean)
+        int idade = 25;
+        double salarioMensal = 3500.50;
+        char genero = 'F';
+        boolean empregadoJava = true;
+
+        System.out.println("Sua idade é: " + idade);
+        System.out.println("Seu salário mensal é: " + salarioMensal);
+        System.out.println("Seu gênero é: " + genero);
+        System.out.println("Empregado em Java: " + empregadoJava);
+    }
+}
