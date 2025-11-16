12a67d72a1e8168eed2e832e96f09bfe08dc09ad
Gabriel <gabriel.nascimento7004@gmail.com>
2025-11-16 00:10:44 -0300

    Update OperadoresAritméticos.java

--- Java/01 - Javanauta/OperadoresAritméticos.java
+++ Java/01 - Javanauta/OperadoresAritméticos.java
@@ -6,5 +6,17 @@ public class OperadoresAritméticos {
         // 3. Multiplicação (*)
         // 4. Divisão (/)
         // 5. Módulo (%)
+        double num1 = 15.5;
+        double num2 = 4.5;
+        double soma = num1 + num2;
+        double subtracao = num1 - num2;
+        double multiplicacao = num1 * num2;
+        double divisao = num1 / num2;
+        double modulo = num1 % num2;
+        System.out.println("Soma: " + soma);
+        System.out.println("Subtração: " + subtracao);
+        System.out.println("Multiplicação: " + multiplicacao);
+        System.out.println("Divisão: " + divisao);
+        System.out.println("Módulo: " + modulo);    
     }
 }
