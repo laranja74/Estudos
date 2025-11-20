fccd687eee58a93b3d3e9cce494152b4eafc20bc
Gabriel <gabriel.nascimento7004@gmail.com>
2025-11-20 00:17:10 -0300

    Update atribuindoValores.java

--- Java/01 - Javanauta/atribuindoValores.java
+++ Java/01 - Javanauta/atribuindoValores.java
@@ -1,19 +1,10 @@
 import java.util.Scanner;
-
 public class atribuindoValores {
     public static void main(String[] args) {
-        Scanner scanner =  new Scanner(System.in);
-        System.out.println("Olá, qual é o seu nome? ");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Olá, qual o seu nome?");
         String nome = scanner.nextLine();
-        System.out.println("Olá " + nome + " seja bem-vindo!");
-
-        System.out.println("Agora me responda qual é a sua idade? ");
-        int idade = scanner.nextInt();
-        System.out.println("" + nome + "" + idade + " anos de idade, Correto?");
-
-        System.out.println("Você está empregado? sim (true) e não (false)");
-        boolean resp = scanner.nextBoolean();
-        System.out.println("Situação: " + resp);
-        scanner.close();
+        System.out.println("Prazer em te conhecer, " + nome + "!");
     }
 }
